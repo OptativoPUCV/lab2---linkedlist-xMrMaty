415291fa2aa8c2df27b60fbb4e0aea45bd5c685d
Replit user <>
2023-08-24 02:46:42 +0000

    new try

--- list.c
+++ list.c
@@ -71,7 +71,16 @@ void * prevList(List * list){
   }
 }
 
-void pushFront(List * list, void * data) {
+void pushFront(List * list, void * data){
+  Node* newNode=createNode(data);
+  if (list->head==NULL){
+    list->head=newNode;
+    list->tail=newNode;
+  }else{
+    newNode->next=list->head;
+    list->head->prev=newNode;
+    list->head=newNode;
+  }
 }
 
 void pushBack(List * list, void * data) {
