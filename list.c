a370dde36bac895c378bba079ba4c801cb4a5364
Replit user <>
2023-08-24 01:04:00 +0000

    new try

--- list.c
+++ list.c
@@ -37,7 +37,7 @@ List * createList(){
 
 void * firstList(List * list){
   list->current=list->head;
-    return &(list->current->head);
+    return &(list->current);
 }
 
 void * nextList(List * list) {
